f85b79c783b5141b673a4580b97f4050afa3b97f
SudiptaSundarPramanik <79316024+SudiptaSundarPramanik@users.noreply.github.com>
2021-06-18 19:44:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ canvas=document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
 car1_width=120;
 car1_height=70;
-car1_image="car1.png";
+car1_image="c2.png";
 car1_x=10;
 car1_y=10;
 car2_width=120;
 car2_height=70;
-car2_image="car2.png";
+car2_image="OIP (1).jpg";
 car2_x=10;
 car2_y=100;
 background_image="racing.jpg";
@@ -17,10 +17,10 @@ function add(){
     background_imgTag.src=background_image;
     car1_imgTag=new Image();
     car1_imgTag.onload=uploadcar1;
-    car1_imgTag.src=car1_imgTag;
+    car1_imgTag.src=car1_image;
     car2_imgTag=new Image();
     car2_imgTag.onload=uploadcar2;
-    car2_imgTag.src=car2_imgTag;
+    car2_imgTag.src=car2_image;
 }
 function uploadBackground(){
     ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
@@ -65,5 +65,85 @@ function my_keydown(e){
     if(keyPressed=='68'){
         car2_right();
         console.log("key d");
+    }
+    if(car1_x>500){
+        console.log("car1 Won");
+        document.getElementById("game_status").innerHTML="Car 1 Won!!";
+    }
+    else if(car2_x>500){
+        console.log("car 2 Won");
+        document.getElementById("game_status").innerHTML="Car 2 Won!!";
+    }
+}
+function car1_up(){
+    if(car1_y>=0){
+        car1_y=car1_y-10;
+        console.log("When up arrow is pressed,x="+car1_x+"|y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car1_down(){
+    if(car1_y<=500){
+        car1_y=car1_y+10;
+        console.log("When down arrow is pressed,x="+car1_x+"|y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car1_left(){
+    if(car1_x>=0){
+        car1_x=car1_x-10;
+        console.log("When left arrow is pressed,x="+car1_x+"|y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car1_right(){
+    if(car1_x<=500){
+        car1_x=car1_x+10;
+        console.log("When down arrow is pressed,x="+car1_x+"|y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_up(){
+    if(car2_y>=0){
+        car2_y=car2_y-10;
+        console.log("When up arrow is pressed,x="+car2_x+"|y="+car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_down(){
+    if(car2_y<=500){
+        car2_y=car2_y+10;
+        console.log("When down arrow is pressed,x="+car2_x+"|y="+car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_left(){
+    if(car2_x>=0){
+        car2_x=car2_x-10;
+        console.log("When left arrow is pressed,x="+car2_x+"|y="+car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_right(){
+    if(car2_x<=500){
+        car2_x=car2_x+10;
+        console.log("When right arrow is pressed,x="+car2_x+"|y="+car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
     }
 }
